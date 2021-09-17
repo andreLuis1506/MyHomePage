c8b16f0d6a3bf88173b659b63e13f3e656cf9ea3
André Luis dos Santos <alsss.ads@gmail.com>
2021-09-17 12:52:36 -0300

    Novo horario

--- db/Horario.js
+++ db/Horario.js
@@ -3,74 +3,50 @@ const date = new Date
 const dias = [
   {
       id: 1,
-      materias: [1,2,3]
+      materias: [1,2]
   },
   {
       id: 2,
-      materias: [6]
+      materias: [3]
   }, 
   {
       id: 3,
-      materias: [1,2,4]
+      materias: [4,3,1]
   },
   {
       id: 4,
-      materias: [3,5,7]
+      materias: []
   },
   {
     id: 5,
-    materias: [1,4,8]
+    materias: []
   }
 ]
 
 const materias = [
   {
     id:1,
-    nome: "programção e padões de projeto",
-    link: "https://classroom.google.com/u/1/c/MjY0MzcyOTM5NTUy",
-    horario: 14
+    nome: "Gestão de T.I.",
+    link: "https://classroom.google.com/u/1/c/Mzc3NTA4OTQwMjI3",
+    horario: date.getDay() == 1 ? 14 : 16
     
   },
   {
     id: 2,
-    nome: "Empreendedorismo",
-    link: "https://classroom.google.com/u/1/c/Mjc5NTQ5MDQ3Mjcy",
-    horario: 15
+    nome: "Topicos em Programção",
+    link: "https://classroom.google.com/u/1/c/Mzc3NjQ0NDc3NTU1",
+    horario: 17
   },
   {
     id: 3,
-    nome: "Redes 2",
-    link: "https://classroom.google.com/u/1/c/Mjc5MDEzOTM5OTY0",
-    horario: date.getDay() == 4 ? 14 : 16
+    nome: "Projeto de Inovação",
+    link: "https://classroom.google.com/u/1/c/Mzc5MTgyMTUxMDYw",
+    horario: date.getDay() == 2 ? 14 : 15
   },
   {
     id: 4,
-    nome: "Engenharia de software 2",
-    link: "https://classroom.google.com/u/1/c/Mjg0Mzc0ODQ1NTg0",
-    horario: date.getDay() == 5 ? 15 : 16
+    nome: "Informatica e Sociedade",
+    link: "https://classroom.google.com/u/1/c/Mzc3OTIwMzk1Njk2",
+    horario: 14
   },
-  {
-    id: 5,
-    nome: "Métodos cientificos",
-    link: "https://classroom.google.com/u/1/c/Mjg1ODA3NzI0NDEx",
-    horario: 15
-  },
-  {
-    id: 6,
-    nome: "Meio ambiente",
-    link: "https://classroom.google.com/u/1/c/Mjg0MTM5ODU4MTgy",
-    horario: 17
-  },
-  {
-    id: 7,
-    nome: "Tópicos em d. de software",
-    link: "https://classroom.google.com/u/1/c/MjY0MzcyOTM5NjAx",
-    horario: 17
-  },
-  {
-    id: 8,
-    nome: "Topicos em redes",
-    link: "https://classroom.google.com/u/1/c/Mjg0Mzc0ODQ1NjA1",
-    horario: 16
-  }
 ]
